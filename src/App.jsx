c6c36a69f048df528a0640bbb7400a363ae5f54f
Eric Smith <sneric.smith@gmail.om>
2024-01-30 21:08:11 -0700

    formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
 import { createRoot } from "react-dom/client";
 
-
 const App = () => {
-  return (
-    <div>
-      App is running on local host
-    </div>
-  );
+  return <div>App is running on local host</div>;
 };
 
 const container = document.getElementById("root");
